f28259e3fb0947a521955cdf6fc7d585eed33ec0
Samin Israr Ravi <saminisrar1@gmail.com>
2022-05-26 03:09:21 +0600

    jwt

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ function verifyJWT(req, res, next) {
   });
 }
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.q4ici.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
